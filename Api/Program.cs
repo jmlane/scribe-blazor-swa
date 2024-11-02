3e0043c52ab0ab53c7f231c93428ccf83c1d75bb
Jonathan M. Lane <jonathan.lane@remsoft.com>
2024-11-02 05:25:05 -0300

    Added CosmosDb to API project

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,14 +1,34 @@
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices((hostContext ,services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-    })
+
+        services.AddAzureClients(clientBuilder =>
+        {
+            clientBuilder.AddClient<CosmosClient, CosmosClientOptions>(
+                options =>
+                {
+                    options.UseSystemTextJsonSerializerWithOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    };
+
+                    return new CosmosClient(
+                        hostContext.Configuration["CosmosDbConnectionString"],
+                        options);
+                });
+                    
+        });
+    }) 
     .Build();
 
 host.Run();
