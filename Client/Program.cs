6cfd987afd0576a502a02589c01997e57fa057b8
Jonathan M. Lane <jonathan.lane@remsoft.com>
2024-11-12 23:56:19 -0400

    Added Authorization to client.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Scribe.Client;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
