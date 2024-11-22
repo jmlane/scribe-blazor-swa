94ff2c50fc737bca1388836d0fd85114fe9de6fe
Jonathan M. Lane <jonathan.lane@remsoft.com>
2024-11-22 20:10:36 +0000

    Removed development appsettings with local API URI
    The authentication and claims principal functionality does not work unless the API is accessed through the SWA proxy.

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string apiUrl = new((builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) + "/api/");
+string apiUrl = new(builder.HostEnvironment.BaseAddress + "api/");
 builder.Services.AddSingleton(new ChatConfig(new Uri(apiUrl + "chat")));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(apiUrl) });
 
